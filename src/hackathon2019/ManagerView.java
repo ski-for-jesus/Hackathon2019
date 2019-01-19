6c17721ca066fb4bf57fda17a71069e74830f3fd
Eric Jaramillo <ejara98@email.arizona.edu>
2019-01-19 00:16:55 -0700

    Added secondary page

--- src/hackathon2019/ManagerView.java
+++ src/hackathon2019/ManagerView.java
@@ -2,6 +2,9 @@ package hackathon2019;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -11,11 +14,30 @@ import javafx.stage.Stage;
 
 public class ManagerView extends Application {
 	public void start(Stage stage) throws Exception {
+		displayMainPage(stage);
+	}
+	
+	private void displayCustomizationMenu(Stage stage) {
+		Canvas menuCanvas = new Canvas(700, 500);
+		BorderPane menuBp = new BorderPane(menuCanvas);
+		Scene menuScene = new Scene(menuBp);
+		Button backButton = new Button("Back");
+		backButton.setOnAction(e -> {
+			displayMainPage(stage);
+		});
+		menuBp.setTop(backButton);
+		stage.setScene(menuScene);
+	}
+	
+	private void displayMainPage(Stage stage) {
 		BorderPane bp = new BorderPane();
 		TilePane tp = new TilePane();
 		Menu createM = new Menu("Create");
 		Menu deleteM = new Menu("Delete");
 		MenuItem newPref = new MenuItem("New Preference Set");
+		newPref.setOnAction(e -> {
+			displayCustomizationMenu(stage);
+		});
 		createM.getItems().add(newPref);
 		MenuBar menuPane = new MenuBar();
 		menuPane.getMenus().add(createM);
