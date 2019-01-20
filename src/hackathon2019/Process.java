d4ffd63bb2af950b46831e2d032868e65badcbe6
ski-for-jesus <tedseipp@gmail.com>
2019-01-20 02:59:11 -0700

    Fixed functionality for mac.

--- src/hackathon2019/Process.java
+++ src/hackathon2019/Process.java
@@ -9,6 +9,7 @@ public class Process {
 	String name;
 	String filePath;
 	boolean browser;
+	boolean checked = false;
 	
 	public Process(String name, String filePath, boolean browser) {
 		
@@ -33,12 +34,29 @@ public class Process {
 			}
 		} else {
 			Runtime rt = Runtime.getRuntime();
+			checkOS();
+			System.out.println(this.filePath);
 			try {
+				
 				rt.exec(this.filePath);
 			} catch(IOException ex) {
 				return false;
 			}
 		}
 		return true;
+	}
+	
+	public void checkOS() {
+		if(checked) return;
+		String os = System.getProperty("os.name").toLowerCase();
+		if(os.indexOf("mac") >= 0) {
+			System.out.println("THIS IS A MAC");
+			String[] temp = this.filePath.split("/");
+			String appName = temp[temp.length - 1];
+			int index = appName.indexOf('.');
+			appName = appName.substring(0, index);
+			this.filePath += "/Contents/MacOS/" + appName;
+			checked = true;
+		}
 	}
 }
